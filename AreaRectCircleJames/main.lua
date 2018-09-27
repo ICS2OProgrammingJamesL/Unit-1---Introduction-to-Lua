fa6ab15772f7831f3a72cd0e437b5dcea02680a5
JamesLyall <43419176+JamesLyall@users.noreply.github.com>
2018-09-27 13:54:27 -0400

    Modified area of rectangle and circle

--- AreaRectCircleJames/main.lua
+++ AreaRectCircleJames/main.lua
@@ -5,56 +5,92 @@
 -- This program displays a rectangle and circle and shows the area
 -----------------------------------------------------------------------------------------
 
--- create my local variables
-local areatext
+-- create my local variables 
+local areaText
 local textSize = 50
 local myRectangle
-local widthOfRectangle = 350
-local heightOfRectangle = 200
+local widthOfRectangle = 500
+local heightOfRectangle = 300
+local areaOfRectangle
 local myCircle
-local textsizeCircle = 40
-local radiusOfCircle = 100
-local areaOfCircle
 local areaTextCircle
-local PI = 3.1415
+local circumference 
+local radiusOfCircle = 140
+local areaOfCircle
+local PI = 3.14
+local AreaText
+local TextSize = 50
 
--- set the background coloue of my screen.remember that colors are between 0 ans 1.
-display.setDefault("background", 200/255, 2/255, 2/255)
+--set the backgorund colour of my screen. Remember that colors are between 0 and 1.
+display.setDefault("background", 29/25, 195/255, 100/255)
 
--- to remove status bar
+--to remove status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- draw the rectangle that is half the width and the height of the screen size.
-myRectangle = display.newRect(0, 0, widthOfRectangle, heightOfRectangle)
+--draw the rectangle that is half the width and height of the screen size.
+myRectangle = display.newRect(10, 10, widthOfRectangle, heightOfRectangle)
 
--- anchor the rectangle in the top left corner of the screen and set its (x,y) position
+--draw the circle that has coordinates of (850,150) and a radius of 140
+myCircle = display.newCircle(850, 150, radiusOfCircle)
+
+--anchor the rectangle in the top left cornor of thge screen and set its (x,y) position
 myRectangle.anchorX = 0
 myRectangle.anchorY = 0
-myRectangle.x = 20
+myRectangle.x = 30
 myRectangle.y = 20
 
--- set the width of the border
+--set the width of the border
 myRectangle.strokeWidth = 20
 
--- set the colour of the rectangle
-myRectangle:setFillColor(0.9, 0.8, 0.3)
+--set the width of the border
+myCircle.strokeWidth = 20
 
--- set the color of the border
-myRectangle:setStrokeColor(0.7, 0.1, 0.5)
+--set the color of the rectangle
+myRectangle:setFillColor(200/255, 100/255,10/255)
 
--- calculate the area
+--set the color of the circle
+myCircle:setFillColor(150/255, 150/255, 200/255)
+
+--set the color of the border
+myRectangle:setStrokeColor(0, 0, 0)
+
+--set the color of the border
+myCircle:setStrokeColor(0, 0, 0)
+
+--calculate the area
 areaOfRectangle = widthOfRectangle * heightOfRectangle
 
--- write the area on the screen. Take into consideration the size of the font when positionign it on the screen.
-areaText = display.newText("The area of this rectangle with a width of \n" ..
-	widthOfRectangle .. "and a height of " .. heightOfRectangle .. " is " ..
-	areaOfRectangle .. " pixels²", 0, 0, Arial, textSize)
+-- calculate the area of the circle
+areaOfCircle = radiusOfCircle * radiusOfCircle * PI
 
--- anchor the text and set its (x,y) position
+
+--write the area on the screen. Take into consideration the size of the font when positioning it on the screen
+areaText = display.newText("The area of this rectangle with a width of \n" ..
+  widthOfRectangle .. " and a height of " .. heightOfRectangle .. " is " ..
+  areaOfRectangle .. " pixels². ", 0, 0, Arial, textSize)
+
+--anchor the text and set its (x,y) position 
 areaText.anchorX = 0
 areaText.anchorY = 0
 areaText.x = 20
 areaText.y = display.contentHeight/2
 
--- set the colour of the newText
-areaText:setTextColor(1, 1, 1)
+-- set the color of the newText 
+areaText:setTextColor (0, 0, 0)
+
+-- set the color of the newText 
+areaText:setTextColor (1, 1, 1)
+
+--Write area of circle on screen.
+AreaText = display.newText("The area of the circle with a radius of \n" ..
+	radiusOfCircle .. " and the pie being " .. PI .. " is " ..
+	areaOfCircle .. "pixels². ", 0, 0, Arial, TextSize)
+
+--anchor the text and set its (x,y) position 
+AreaText.anchorX = 0
+AreaText.anchorY = 0
+AreaText.x = 20
+AreaText.y = display.contentHeight*2/3
+
+
+
