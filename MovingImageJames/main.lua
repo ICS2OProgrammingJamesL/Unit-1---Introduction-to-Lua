3f796dc1726623f1124827d20836d238000e1d5d
JamesLyall <43419176+JamesLyall@users.noreply.github.com>
2018-10-03 13:50:32 -0400

    added TouchDrag

--- MovingImageJames/main.lua
+++ MovingImageJames/main.lua
@@ -1,7 +1,36 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: MovingObjects
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program moves a beetleship across the screen and then makes it fade out.
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 5
+-- background image with width snd height
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+
+-- character image with width and height
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- set the image to be transparent
+beetleship.alpha = 0
+
+-- set the initial x and y position of beetleship
+beetleship.x = 400
+beetleship.y = 0
+
+-- function: MovieShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to the x-value of the ship
+	beetleship.y = beetleship.y + scrollSpeed
+	--change the transparency of the ship every time it moves so that it fades out
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+-- MoveShip will be called over and overagain
+Runtime:addEventListener("enterFrame", MoveShip)
