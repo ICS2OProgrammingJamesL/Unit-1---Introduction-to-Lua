8dcebe0c31dc5769be7160ee20ac7df36f57ed9a
JamesLyall <43419176+JamesLyall@users.noreply.github.com>
2018-10-22 14:30:54 -0400

    Updated numeric text fields

--- NumericTextFieldsJamesL/main.lua
+++ NumericTextFieldsJamesL/main.lua
@@ -23,10 +23,75 @@ local userAnswer
 local correctAnswer
 local incorrectAnswer
 local randomOperator
+
+-- Points variables
 local Points = 0
-local pointsTextObject
+local pointsText
+
+-- Variables for the timer.
+local totalSeconds = 5
+local secondsLeft = 5
+local clockText
+local countDownTimer
+
+-- variables for lives.
+local lives = 4
+local heart1
+local heart2
+local heart3
+local heart4
 
 
+
+-- function that calls the timer
+local function StartTimer()
+	-- create a countdown timer that loops infinitely
+	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+end
+
+-- create the lives to display on the screen
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("Images/heart.png", 100, 100)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+
+-- local function for timer and lives
+local function UpdateTime()
+
+	-- decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+
+	-- display the number of seconds left in the clock object
+	clockText.text = secondsLeft .. ""
+
+	if (secondsLeft == 0 ) then
+		-- reset the number of seconds left
+		secondsLeft = totalSeconds
+		lives = lives - 1
+		
+		if (lives == 4) then
+			heart4.isVisible = false
+		elseif (lives == 3) then
+			heart3.isVisible = false
+		elseif (lives == 2) then
+			heart2.isVisible = false
+		elseif (lives == 1) then
+			heart1.isVisible = false
+		end
+	end
+end
 
 
 -- local functions
@@ -92,7 +157,8 @@ local function NumericFieldListener( event )
 			event.target.text = ""
 			timer.performWithDelay(2000, HideCorrect)
 			Points = Points + 1
-			else
+			pointsText.text = " Points = " .. Points
+		else
 			incorrectObject.isVisible = true
 			event.target.text = ""
 			correctObject.isVisible = false	
@@ -102,8 +168,8 @@ local function NumericFieldListener( event )
 end
 
 -- display points text and colour it
-pointsTextObject = display.newText( " Points = " .. Points, 100, 100, nil, 50)
-pointsTextObject:setTextColor(10/255, 100/255, 10/255)
+pointsText = display.newText( " Points = " .. Points, 100, 100, nil, 50)
+pointsText:setTextColor(10/255, 100/255, 10/255)
 
 -- OBJECT CREATION
 
